7a194120d00b5169d5200072ae69b691826646f3
Anne Fausto <a.r.fausto@gmail.com>
2019-09-19 19:32:43 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,20 +1,6 @@
 require "pry"
 def oxford_comma(array)
- #array*(" and ")
- 
-#else
-   #new_array = []
-   #counter = 1
-   #array.each do |i|
-   #new_array << "#{i}, "
-   #counter += 1
-  #  end
- #new_array.insert(-2,"and ")
- #new_array[0..new_array.length-1].join
- 
- new_array.join
- 
-  if array.length == 1
+ if array.length == 1
     array[0]
   elsif array.length == 2
     array.insert(1," and ")
