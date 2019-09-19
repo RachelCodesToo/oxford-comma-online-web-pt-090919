5bd42efac98f83810a79237ad353e4c75f15766f
Anne Fausto <a.r.fausto@gmail.com>
2019-09-19 19:30:58 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -16,7 +16,7 @@ def oxford_comma(array)
  
   if array.length == 1
     array[0]
- elsif array.length == 2
+   elsif array.length == 2
     array.insert(1," and ")
     array.join
   else array.length >= 3
